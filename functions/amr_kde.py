64a688714cf1c925842764939d552a32ea86d191
Gabriel-p <gabrielperren@gmail.com>
2016-05-27 13:22:41 -0300

    make average integral [Fe/H] optinal

--- functions/amr_kde.py
+++ functions/amr_kde.py
@@ -6,7 +6,7 @@ from scipy.integrate import simps
 
 
 def age_met_rel(xarr, xsigma, yarr, ysigma, grid_step):
-    '''
+    """
     Generate a grid in the age-metallicity diagram.
     1- Obtain the 2D KDE of the age-metallicity parameters space.
     2- Obtain a unique [Fe/H] value for each age value in the grid, *weighted*
@@ -14,7 +14,7 @@ def age_met_rel(xarr, xsigma, yarr, ysigma, grid_step):
     3- Obtain the error associated to that [Fe/H] value.
 
     See: http://math.stackexchange.com/q/1457390/37846
-    '''
+    """
 
     # Define 2D space extension where the KDE will be obtained.
     x_min, x_max = min(np.array(xarr) - np.array(xsigma)), \
@@ -72,82 +72,18 @@ def age_met_rel(xarr, xsigma, yarr, ysigma, grid_step):
     return age_vals, met_weighted
 
 
-def get_amr_asteca(in_params):
+def feh_avrg_integ(age_gyr, bn, age_vals, met_weighted):
     """
-    Obtain AMR for both MCs. Steps:
-
-    0- Filter OCs if necessary, for testing.
-    1- Add old LMC OCs if necessary, for testing.
-    2- Convert log(ages) to Age (Gyr) (same for errors)
-    3- Define values for grid_step and binning. The latter is the one that will
-       affect the AMR the most. The former just needs to be small enough to
-       allowing sampling a fine grid in the age-metallicity space.
-    4- Obtain equispaced age values in grid, and equispaced *weighted* [Fe/H]
-       values in grid (age_vals, met_weighted).
-    5- Obtain bin edges for the entire age range.
-    6- For each age range, obtain the average integral of the weighted [Fe/H],
+    1- Obtain bin edges for the entire age range.
+    2- For each age range, obtain the average integral of the weighted [Fe/H],
        using Simpson's rule. The associated age value is the mid point of the
        age range.
-    7- Use Monte Carlo to calculate errors for the representative [Fe/H] value
+    3- Use Monte Carlo to calculate errors for the representative [Fe/H] value
        for that age range.
     """
 
-    zarr, zsigma, aarr, asigma = [
-        in_params[_] for _ in ['zarr', 'zsigma', 'aarr', 'asigma']]
-
-    # First index k indicates the galaxy (0 for SMC, 1 for LMC), the second
-    # index 0 indicates ASteCA values.
-    # k=0 -> SMC, k=1 ->LMC
-    age_gyr, age_vals, met_weighted, feh_f, age_rang_MCs =\
-        [[], []], [[], []], [[], []], [[], []], [[], []]
+    age_vals_int, met_weighted_int, age_rang_MCs = [[], []], [[], []], [[], []]
     for k in [0, 1]:
-        # Exclude OCs with extremely low metallicities.
-        age_f, age_err_f, feh_err_f = [], [], []
-        for v in zip(*[aarr[k][0], asigma[k][0], zarr[k][0], zsigma[k][0]]):
-            # To filter out HW85 and NGC294
-            # if v[2] > -1.5:
-            # To filter out the 4 LMC OCs with large ages ans metallicities.
-            # if not (9.48 < v[0] < 9.6 and v[2] > -0.3):
-            # To include all OCs.
-            if True:
-                age_f.append(v[0])
-                age_err_f.append(v[1])
-                feh_f[k].append(v[2])
-                feh_err_f.append(v[3])
-            else:
-                print v
-
-        # #####################################################################
-        # # Add old LMC OCs taken from Piatti & Geisler (2013)
-        # old_lmc_OCs = [[9.93, 10.09, 10.13, 10.11, 10.13, 10.09, 10.13,
-        #                 10.15, 10.17, 10.09, 10.09, 10.09],
-        #                [-1.049, -1.202, -1.369, -1.418, -1.62, -1.746,
-        #                 -1.847, -1.865, -2.049, -1.997, -2.095, -2.196]]
-        # if k == 1:
-        #     age_f = age_f + old_lmc_OCs[0]
-        #     feh_f[k] = feh_f[k] + old_lmc_OCs[1]
-        #     # Add a reasonable but small error.
-        #     age_err_f = age_err_f + [0.05]*len(old_lmc_OCs[0])
-        #     feh_err_f = feh_err_f + [0.05]*len(old_lmc_OCs[0])
-        # #####################################################################
-
-        # Age in Gyrs.
-        age_gyr[k] = [10 ** (np.asarray(age_f) - 9),
-                      np.asarray(age_err_f) * np.asarray(age_f) *
-                      np.log(10) / 5.]
-
-        # THESE NUMBERS AFFECT THE SHAPE OF THE FINAL AMR.
-        # Grid step.
-        grid_step = 0.01
-        # Define method or number of bins for the age range.
-        bn = 'knuth'
-
-        # Weighted metallicity values for an array of ages.
-        # Max limit on very large met errors.
-        zsig = [min(2., _) for _ in feh_err_f]
-        age_vals[k], met_weighted[k] = age_met_rel(
-            age_gyr[k][0], age_gyr[k][1], feh_f[k], zsig, grid_step)
-
         # import matplotlib.pyplot as plt
         # f, (ax1, ax2) = plt.subplots(1, 2)
         # ax1.scatter(age_vals[k], met_weighted[k][0])
@@ -213,8 +149,91 @@ def get_amr_asteca(in_params):
                 pass
         # plt.show()
         # Store AMR function values.
-        age_vals[k], met_weighted[k] = age_temp, [met_temp, met_err_temp]
-        age_rang_MCs[k] = age_rang
+        age_vals_int[k], met_weighted_int[k], age_rang_MCs[k] =\
+            age_temp, [met_temp, met_err_temp], age_rang
+
+    return age_vals_int, met_weighted_int, age_rang_MCs
+
+
+def get_amr_asteca(in_params):
+    """
+    Obtain AMR for both MCs. Steps:
+
+    0- Filter OCs if necessary, for testing.
+    1- Add old LMC OCs if necessary, for testing.
+    2- Convert log(ages) to Age (Gyr) (same for errors)
+    3- Define values for grid_step and binning. The latter is the one that will
+       affect the AMR the most. The former just needs to be small enough to
+       allowing sampling a fine grid in the age-metallicity space.
+    4- Obtain equispaced age values in grid, and *weighted* [Fe/H]
+       values in grid (age_vals, met_weighted).
+    5- Call function to obtain an average integral [Fe/H] value for each age
+       range (along with its Monte Carlo error)
+    """
+
+    zarr, zsigma, aarr, asigma = [
+        in_params[_] for _ in ['zarr', 'zsigma', 'aarr', 'asigma']]
+
+    # First index k indicates the galaxy (0 for SMC, 1 for LMC), the second
+    # index 0 indicates ASteCA values.
+    # k=0 -> SMC, k=1 ->LMC
+    age_gyr, age_vals, met_weighted, feh_f =\
+        [[], []], [[], []], [[], []], [[], []]
+    for k in [0, 1]:
+        # Exclude OCs with extremely low metallicities.
+        age_f, age_err_f, feh_err_f = [], [], []
+        for v in zip(*[aarr[k][0], asigma[k][0], zarr[k][0], zsigma[k][0]]):
+            # To filter out HW85 and NGC294
+            # if v[2] > -1.5:
+            # To filter out the 4 LMC OCs with large ages ans metallicities.
+            # if not (9.48 < v[0] < 9.6 and v[2] > -0.3):
+            # To include all OCs.
+            if True:
+                age_f.append(v[0])
+                age_err_f.append(v[1])
+                feh_f[k].append(v[2])
+                feh_err_f.append(v[3])
+            else:
+                print v
+
+        # #####################################################################
+        # # Add old LMC OCs taken from Piatti & Geisler (2013)
+        # old_lmc_OCs = [[9.93, 10.09, 10.13, 10.11, 10.13, 10.09, 10.13,
+        #                 10.15, 10.17, 10.09, 10.09, 10.09],
+        #                [-1.049, -1.202, -1.369, -1.418, -1.62, -1.746,
+        #                 -1.847, -1.865, -2.049, -1.997, -2.095, -2.196]]
+        # if k == 1:
+        #     age_f = age_f + old_lmc_OCs[0]
+        #     feh_f[k] = feh_f[k] + old_lmc_OCs[1]
+        #     # Add a reasonable but small error.
+        #     age_err_f = age_err_f + [0.05]*len(old_lmc_OCs[0])
+        #     feh_err_f = feh_err_f + [0.05]*len(old_lmc_OCs[0])
+        # #####################################################################
+
+        # Age in Gyrs.
+        age_gyr[k] = [10 ** (np.asarray(age_f) - 9),
+                      np.asarray(age_err_f) * np.asarray(age_f) *
+                      np.log(10) / 5.]
+
+        # Grid step.
+        grid_step = 0.01
+
+        # Weighted metallicity values for an array of ages.
+        # Max limit on very large met errors.
+        zsig = [min(2., _) for _ in feh_err_f]
+        age_vals[k], met_weighted[k] = age_met_rel(
+            age_gyr[k][0], age_gyr[k][1], feh_f[k], zsig, grid_step)
+
+    # THIS NUMBER WILL AFFECT THE SHAPE OF THE FINAL AMR.
+    # Define method or number of bins for the age range.
+    bn = 'knuth'
+    # Obtain average integral [Fe/H] value for each age range.
+    age_vals, met_weighted, age_rang_MCs = feh_avrg_integ(
+        age_gyr, bn, age_vals, met_weighted)
+    #
+    # # Replace the above by this line to skip the calculation of the average
+    # # integral [Fe/H], and just keep the weighted values.
+    # age_rang_MCs = [np.arange(-2., -1., 0.1), np.arange(-2., -1., 0.1)]
 
     amr_asteca = [age_vals, met_weighted, age_gyr, feh_f, age_rang_MCs]
     return amr_asteca
